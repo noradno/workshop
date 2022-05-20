f082490e800be8c3c2f623349e10bf1f6316e24a
einartornes <45436606+einartornes@users.noreply.github.com>
2022-05-20 16:03:50 +0200

    updates

--- del1.R
+++ del1.R
@@ -10,10 +10,11 @@ library(writexl)
 # Read aid data into R environment
 df <- noradstats::read_aiddata("statsys_ten.csv")
 
-glimpse(df)
+df <- noradstats::add_cols_basic(df)
 
-df <- noradstats::add_cols_basic(df) |> 
-  janitor::clean_names()
+df <- janitor::clean_names(df)
+
+glimpse(df)
 
 
 # Top ten countries 2021 --------------------------------------------------
@@ -26,6 +27,30 @@ df_countries <- df |>
   group_by(recipient_country_no) |> 
   summarise(nok_mill = sum(disbursed_mill_nok)) |> 
   slice_max(nok_mill, n = 10)
+
+df_partner <- df |> 
+  filter(type_of_flow == "ODA") |> 
+  filter(type_of_agreement != "Rammeavtale") |> 
+  filter(year == max(year)) |> 
+  group_by(partner_group_visual_no) |> 
+  summarise(nok_mill = sum(disbursed_mill_nok)) |>
+  arrange(desc(nok_mill))
+
+# Figur
+
+df_countries |> 
+  ggplot(aes(x = fct_reorder(recipient_country_no, nok_mill), y = nok_mill)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  geom_text(aes(label = nok_mill), hjust = 0) +
+  labs(
+    title = paste("Største mottakerland i", max(df$year)),
+    subtitle = "Beløp i millioner kroner",
+    x = NULL,
+    y = "Millioner kroner"
+  ) +
+  theme_minimal()
+  
 
 
 # Save to excel file ------------------------------------------------------
--- del2.R
+++ del2.R
@@ -20,7 +20,7 @@ df <- noradstats::add_cols_basic(df) |>
 df_countries <- df |> 
   filter(type_of_flow == "ODA") |> 
   filter(type_of_agreement != "Rammeavtale") |> 
-  filter(year == 2020) |> 
+  filter(year == 2019) |> 
   filter(income_category != "Unspecified") |> 
   group_by(recipient_country_crs, recipient_country_no) |> 
   summarise(earmarked_nok_mill = sum(disbursed_mill_nok)) |> 
@@ -30,7 +30,7 @@ df_countries <- df |>
 
 # Imputed multilateral ODA to countries -----------------------------------
 
-df_imputed <- noradstats::get_imputed(startyear = 2020, endyear = 2020)
+df_imputed <- noradstats::get_imputed(startyear = 2018, endyear = 2018)
 
 glimpse(df_imputed)
 
@@ -45,19 +45,18 @@ df_imputed <- df_imputed %>%
 
 # Merge datasets of earmarked and imputed ---------------------------------
 
-df_total <- left_join(df_countries, df_imputed, by = c("recipient_country_crs" = "recipient"))
-
-glimpse(df_total)
+df_total <- full_join(df_countries, df_imputed, by = c("recipient_country_crs" = "recipient"))
 
 df_total <- df_total |> 
-  select(recipient_country_no, earmarked_nok_mill, imputed_nok_mill) |> 
+  select(recipient_country_crs, recipient_country_no, recipient_label_en, earmarked_nok_mill, imputed_nok_mill) |> 
+  mutate(recipient_country_crs = as.character(recipient_country_crs)) |> 
   adorn_totals("col") |> 
   arrange(desc(Total))
 
 # Save table in excel file ------------------------------------------------------
 
-writexl::write_xlsx(df_total, "del2.xlsx")
+writexl::write_xlsx(list("2020" = df_total2020, "2019" = df_total2019, "2018" = df_total2018), "land.xlsx")
 
-
+df_total2018 <- df_total
 
 
